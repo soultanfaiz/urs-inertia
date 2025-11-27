f3e023f094144f8069f6e8d0e96fd76a623bd8f9
soultanfaiz <soultanfaiz@gmail.com>
2025-11-27 15:23:36 +0800

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,24 +4,16 @@ import './bootstrap';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
-import { ZiggyVue } from './ziggy';
+import { ZiggyVue } from 'ziggy-js';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./Pages/${name}.vue`,
-            import.meta.glob('./Pages/**/*.vue'),
-        ),
-    setup({ el, App, props, plugin }) {
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props }) {
         return createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(ZiggyVue)
+            .use(ZiggyVue, props.initialPage.props.ziggy) // Use the ZiggyVue plugin
             .mount(el);
-    },
-    progress: {
-        color: '#4B5563',
     },
 });
